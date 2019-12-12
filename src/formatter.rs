fc8345b3b652ea6b608cb0f180237bc209e4ffc0
Aster <galaster@foxmail.com>
2019-12-12 20:21:28 +0800

    🌿 Format grammar rule

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,5 +1,6 @@
 use crate::grammar::{PestParser, Rule};
-use pest::Parser;
+use pest::{Parser, Span};
+use pest::iterators::Pair;
 
 #[derive(Debug)]
 pub struct Settings {
@@ -8,10 +9,64 @@ pub struct Settings {
 }
 
 impl Settings {
-    pub fn format(self, text: &str) -> String {
+    pub fn format_file(&self, path: &str) -> String {
+        return String::new();
+    }
+    pub fn format(&self, text: &str) -> String {
         let pairs = PestParser::parse(Rule::grammar_rules, text).unwrap_or_else(|e| panic!("{}", e));
         let mut code = String::new();
         for pair in pairs {
+            match pair.as_rule() {
+                Rule::EOI => {
+                    if self.pest_end_line { code.push_str("\n") }
+                }
+                Rule::grammar_rule => {
+                    let out = self.format_grammar_rule(pair);
+                    code.push_str(&out)
+                }
+                _ => unreachable!()
+            };
+        }
+        unreachable!();
+        return code;
+    }
+    fn format_grammar_rule(&self, pairs: Pair<Rule>) -> String {
+        let mut code = String::new();
+        let mut modifier = " ";
+        let mut id = "";
+        let one_line = is_one_line(pairs.as_span());
+        for pair in pairs.into_inner() {
+            match pair.as_rule() {
+                Rule::assignment_operator => continue,
+                Rule::opening_brace => continue,
+                Rule::closing_brace => continue,
+                Rule::identifier => {
+                    id = pair.as_str()
+                }
+                Rule::silent_modifier => {
+                    modifier = pair.as_str()
+                }
+                Rule::atomic_modifier => {
+                    modifier = pair.as_str()
+                }
+                Rule::compound_atomic_modifier => {
+                    modifier = pair.as_str()
+                }
+                _ => {
+                    println!("Rule:    {:?}", pair.as_rule());
+                    println!("Span:    {:?}", pair.as_span());
+                    println!("Text:    {}\n", pair.as_str());
+                }
+            };
+        }
+        return code;
+    }
+    fn format_expression(&self, pairs: Pair<Rule>) -> String {
+        let mut code = String::new();
+        let mut modifier = " ";
+        let mut id = "";
+        let one_line = is_one_line(pairs.as_span());
+        for pair in pairs.into_inner() {
             match pair.as_rule() {
                 _ => {
                     println!("Rule:    {:?}", pair.as_rule());
@@ -20,10 +75,12 @@ impl Settings {
                 }
             };
         }
-        unreachable!();
         return code;
     }
-    pub fn format_file(self, path: &str) -> String {
-        return String::new();
-    }
+}
+
+fn is_one_line(span: Span) -> bool {
+    let s = span.start_pos().line_col().0;
+    let e = span.end_pos().line_col().0;
+    return s == e;
 }
